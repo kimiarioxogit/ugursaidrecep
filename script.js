6f38c71b830b3821188dcf46e4846a38f7e022e1
RecepKandemir <kandemirrecep06@gmail.com>
2025-10-01 12:54:29 +0300

    oluyormu

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  const name = "JAQLIV";
+  const name = "ghghg";
   let nameText = '';
   let nameIndex = 0;
   let isNameDeleting = false;
